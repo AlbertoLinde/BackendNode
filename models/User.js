3e49557dc91612233c214f153bf59799efbeb3cf
abreu <abreulindealberto@gmail.com>
2022-06-11 11:48:13 +0100

    Added encryption for password before save in DB

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const {Schema, model} = mongoose;
 
@@ -16,5 +17,21 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+// Before save anything use PRE
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next;
+
+    try {
+        const jumps = await bcrypt.genSalt(10);
+        user.password = bcrypt.hash(user.password, jumps);
+        next();
+    } catch (e) {
+        console.log(e);
+        throw new Error('[ERROR] - BCrypt Password generation failed.')
+    }
+});
+
 
 export const User = model('user', userSchema);
